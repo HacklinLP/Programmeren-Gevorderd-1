e894e6b052b509fdb961e7ae85c8e79e1c7e2a69
Lucas Petit <lucas.petit@hotmail.com>
2025-09-29 08:25:14 +0200

    1

---
+++ Opgave Bedrijven def/BedrijvenDomein/Bedrijf.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BedrijvenDomein
+{
+    public class Bedrijf
+    {
+        public Bedrijf(string naam, string sector, string industrie, string extraInfo, string hoofdkwartier, int jaarOprichting, List<Personeel> personeel)
+        {
+            Naam = naam;
+        }
+
+        public string Naam { get; set; }
+        public string Sector { get; set; }
+        public string Industrie { get; set; }
+
+        public int jaarOprichting { get; set; }
+        
+        public List<Personeel> Personeel { get; set; }
+    }
+}
